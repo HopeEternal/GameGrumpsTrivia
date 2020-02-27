b548580c12f05b5a695cb4f7df4c85b71fd2381a
HopeEternal <risalee316@gmail.com>
2020-02-26 21:43:57 -0500

    Trivia Question logic completed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,20 @@
 <template>
   <v-container>
-    <h1>HelloWorld</h1>
+    <h1 class="orange--text text--darken-4">Measure your Game Grumps cred!</h1>
+    <v-card v-for="(question, index) in questions" :key="index" class="pa-5 ma-8">
+      <v-card-title>Question {{index +1}}</v-card-title>
+      <v-card-text>{{question.question}}</v-card-text>
+      <v-card-actions>
+        <v-radio-group class="ms-5">
+          <v-radio
+            v-for="(answer, index) in question.answers"
+            :key="index"
+            :label="`${answer}`"
+            :value="index"
+          ></v-radio>
+        </v-radio-group>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
@@ -8,6 +22,38 @@
 export default {
   name: "HelloWorld",
 
-  data: () => ({})
+  data: () => ({
+    questions: [
+      {
+        category: "Quotes and Memes",
+        question:
+          "After stating \"I don't like Nintendo anymore, it's for babies\", what version of Call of Duty does Arin proclaim that he wants to play?",
+        answers: [
+          "COD: Pew Pew",
+          "COD: Shoot a Man",
+          "COD: Missed",
+          "COD: Plus Ultra"
+        ],
+        correct: "2",
+        source: "https://www.youtube.com/watch?v=f_fDKl0dHmA"
+      },
+      {
+        category: "Quotes and Memes",
+        question:
+          "According to Arin, what is the name of a disease you get from chewing too much?",
+        answers: ["Twilt", "Spoodles", "Sprankton", "Tamould"],
+        correct: "3",
+        source: "https://www.youtube.com/watch?v=xrbdfeva2bM"
+      },
+      {
+        category: "Quotes and Memes",
+        question:
+          "According to a Grumps bit, Ashman's life cleaned up after meeting which invented mega-man character?",
+        answers: ["Splashman", "Trashman", "Stashman", "Splashman"],
+        correct: "2",
+        source: "https://www.youtube.com/watch?v=uUILjiniT88"
+      }
+    ]
+  })
 };
 </script>
