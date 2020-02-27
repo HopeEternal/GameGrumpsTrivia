2bdc87ad86b533a2852f15b60b1b1c2c7fdb99e0
HopeEternal <risalee316@gmail.com>
2020-02-26 20:40:39 -0500

    Vuetify basic site layout completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app color="orange darken-4" dark>
+      <v-icon x-large class="pa-3">mdi-gamepad-variant</v-icon>
+      <v-toolbar-title>
+        <h2 class="font-weight-thin">Game Grumps Trivia!</h2>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <nav class="white" light>
+        <v-btn text>
+          <router-link to="/">Home</router-link>
+        </v-btn>
+        <v-btn text>
+          <router-link to="/about">About</router-link>
+        </v-btn>
+      </nav>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-content>
+          <router-view />
+        </v-content>
+      </v-container>
+    </v-content>
+    <v-footer color="orange darken-4" app>
+      <span class="white--text">&copy; 2020</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss"></style>
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  })
+};
+</script>
+
+<style>
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,13 @@
 <template>
-  <v-app>
-    <v-app-bar app color="indigo" dark>
-      <div class="d-flex align-center text-center">
-        <v-icon large>mdi-checkbox-multiple-marked-circle-outline</v-icon>
-        <h1 class="font-weight-light text-right">Just Do It!</h1>
-      </div>
-    </v-app-bar>
-  </v-app>
+  <v-container>
+    <h1>HelloWorld</h1>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: "HelloWorld",
+
+  data: () => ({})
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
