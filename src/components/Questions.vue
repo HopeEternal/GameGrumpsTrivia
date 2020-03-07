600c534b8d88389d752e7f70aaea6047ee02583b
HopeEternal <risalee316@gmail.com>
2020-03-06 19:44:08 -0500

    Added logic to check for correct answer

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -10,9 +10,13 @@
             :key="index"
             :label="`${answer}`"
             :value="index"
+            @click="checkAnswer(question, index)"
           ></v-radio>
         </v-radio-group>
       </v-card-actions>
+      <div v-if="question.answeredCorrectly !== ''">
+        <p>{{question.answeredCorrectly}}</p>
+      </div>
     </v-card>
   </v-container>
 </template>
@@ -33,26 +37,39 @@ export default {
           "COD: Missed",
           "COD: Plus Ultra"
         ],
-        correct: "2",
-        source: "https://www.youtube.com/watch?v=f_fDKl0dHmA"
+        correct: 2,
+        source: "https://www.youtube.com/watch?v=f_fDKl0dHmA",
+        answeredCorrectly: ""
       },
       {
         category: "Quotes and Memes",
         question:
           "According to Arin, what is the name of a disease you get from chewing too much?",
         answers: ["Twilt", "Spoodles", "Sprankton", "Tamould"],
-        correct: "3",
-        source: "https://www.youtube.com/watch?v=xrbdfeva2bM"
+        correct: 3,
+        source: "https://www.youtube.com/watch?v=xrbdfeva2bM",
+        answeredCorrectly: ""
       },
       {
         category: "Quotes and Memes",
         question:
           "According to a Grumps bit, Ashman's life cleaned up after meeting which invented mega-man character?",
-        answers: ["Splashman", "Trashman", "Stashman", "Splashman"],
-        correct: "2",
-        source: "https://www.youtube.com/watch?v=uUILjiniT88"
+        answers: ["Splashman", "Trashman", "Stashman", "Mashman"],
+        correct: 2,
+        source: "https://www.youtube.com/watch?v=uUILjiniT88",
+        answeredCorrectly: ""
       }
     ]
-  })
+  }),
+  methods: {
+    checkAnswer(question, index) {
+      // console.log(question + index);
+      if (index === question.correct - 1) {
+        question.answeredCorrectly = "That is correct!";
+      } else {
+        question.answeredCorrectly = "Sorry, that's incorrect!";
+      }
+    }
+  }
 };
 </script>
