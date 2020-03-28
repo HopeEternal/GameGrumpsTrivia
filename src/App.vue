f8187cba2ac930c564abd9a9e462d6ab077eb469
HopeEternal <risalee316@gmail.com>
2020-03-28 12:45:40 -0400

    QuizBar component removed and merged with AppBar in App component

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,14 @@
         <h2 class="font-weight-thin">Game Grumps Trivia!</h2>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <QuizBar />
+      <v-icon class="px-4">mdi-account</v-icon>
+      <h3>{{quizSettings.name}}</h3>
+      <v-icon class="px-4 ml-8">mdi-chess-rook</v-icon>
+      <h3>{{quizSettings.difficultyLevel}}</h3>
+      <v-icon class="px-4 ml-8">mdi-timer</v-icon>
+      <h3>2:00</h3>
+      <v-icon class="px-4 ml-8">mdi-trophy</v-icon>
+      <h3>Score: 00</h3>
     </v-app-bar>
 
     <v-content>
@@ -23,17 +30,17 @@
 </template>
 
 <script>
-import QuizBar from "@/components/QuizBar.vue";
 export default {
   name: "App",
 
-  components: {
-    QuizBar
-  },
-
   data: () => ({
     //
-  })
+  }),
+  computed: {
+    quizSettings() {
+      return this.$store.state.quizSettings;
+    }
+  }
 };
 </script>
 
